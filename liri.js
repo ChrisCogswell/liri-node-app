43ba46b0874c7a923d023d2e421c8af0a7d640ef
ChrisCogs <ccogs@yahoo.com>
2019-04-26 21:25:38 -0400

    Began work with omdb functionality. Issues with spotify to figure out

--- liri.js
+++ liri.js
@@ -1,7 +1,32 @@
 require("dotenv").config();
 
 var fs = require("fs");
-
+var axios = require("axios");
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+let action = process.argv[2]
+
+switch (action) {
+    case "movie-this":
+    movieThis();
+    break;
+}
+
+
+
+
+function movieThis() {
+var movieTitle = process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Release Year: " + response.data.Year);
+  }
+);
+}
