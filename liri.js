b7670f00eec63e8fff129732d1af4bae007b60d2
ChrisCogs <ccogs@yahoo.com>
2019-04-28 20:11:01 -0400

    Have movie-this working. Started work on concert-this

--- liri.js
+++ liri.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 
 var fs = require("fs");
 var axios = require("axios");
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: "e148499be3944bda99b5c26ae9f5279c",
-    secret: "f75187e63b0e4f7b897a22c5eca2cbca"
-  });
-console.log(spotify);
+// var spotify = new Spotify({
+//     id: "e148499be3944bda99b5c26ae9f5279c",
+//     secret: "f75187e63b0e4f7b897a22c5eca2cbca"
+//   });
+// console.log(spotify);
 
-let action = process.argv[2]
+let action = process.argv[2];
 
 switch (action) {
     case "movie-this":
@@ -32,34 +32,64 @@ switch (action) {
 }
 
 
-
+//<------------------ OMDB Search --------------------->
 
 function movieThis() {
-var movieTitle = process.argv[3];
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+   var nodeArgs = process.argv;
 
-// console.log(queryUrl);
+   var movieTitle = "";
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Title: " + response.data.Title);
-    console.log("Release Year: " + response.data.Year);
-    console.log("Imdb Rating: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
-    console.log("Country of origin: " + response.data.Country);
-    console.log("Language: " + response.data.Language);
-    console.log("Plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
-  }
-);
+   for (var i = 3; i < nodeArgs.length; i++){
+       if (i > 3 && i < nodeArgs.length){
+           movieTitle = movieTitle + "+" + nodeArgs[i];
+       }
+       else {
+           movieTitle += nodeArgs[i];
+       }
+   }   
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+            axios.get(queryUrl).then(
+              function(response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Release Year: " + response.data.Year);
+                console.log("Imdb Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
+                console.log("Country of origin: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+              }
+            );
 }
 
+//<------------------ Bands in Town Search --------------------->           
 
 function concertThis(){
 
+    var artistName = process.argv[3];
 
-}
+     axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+                
+                console.log("---------------------------------------------");
+                console.log("Venue name: " + response.data[i].venue.name);
+                console.log("Venue city: " + response.data[i].venue.city);
+                console.log("Concert date: " + response.data[i].datetime);
+                console.log("---------------------------------------------");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+    
+      
+
+
 
 
 function spotifyThis(){
